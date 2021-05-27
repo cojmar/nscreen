14cf20da96fdb527bee7859a18d17acd58d8f36a
cojmar <cojmar@gmail.com>
2021-05-27 23:37:24 +0300

    ..

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ new class {
 		let canvasContext = this.canvas.getContext("2d");
 		canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
 		canvasContext.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
-		return this.canvas.toDataURL('image/jpg', 0.7);
+		return this.canvas.toDataURL('image/jpg', 1);
 	}
 	uuid() {
 		let d = new Date().getTime();
