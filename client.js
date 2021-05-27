017d160bb033eb52de5f4204e82c44d8d7ab1bbc
cojmar <cojmar@gmail.com>
2021-05-27 21:37:43 +0300

    .

--- client.js
+++ client.js
@@ -1,16 +1,16 @@
 new class {
 	constructor() {
-		this.my_room = atob(window.location.href.split('/').pop())
+		this.my_room = (window.location.href.indexOf('#') !== -1) ? atob(window.location.href.split('#').pop()) : atob(window.location.href.split('/').pop())
 		import (`./network.js`).then((module) => {
 			this.net = new module.default();
 			this.net.on('connect', () => {
 				this.net.send_cmd('auth', { 'user': '', 'room': this.my_room })
 			})
 			this.net.on('auth.info', () => {
-				this.start()
+
 			})
-			this.net.on('img_frame', (img) => {
-				document.getElementById('out').src = img
+			this.net.on('img_frame', (msg) => {
+				document.getElementById('out').src = msg.data
 			})
 			this.net.connect('wss://ws.emupedia.net')
 		})
