f3f8b2b7b89ed6a793fb56865d8a8ee5ce34d8da
cojmar <cojmar@gmail.com>
2021-05-29 20:54:08 +0300

    .

--- network.js
+++ network.js
@@ -44,6 +44,9 @@ export default class {
 		return this;
 	}
 	getBaseUrl() {
+		if (typeof run_mode !== 'undefined') {
+			if (!run_mode.main) return false;
+		}
 		if (typeof window === 'undefined') return 'localhost';
 		return window.location.href.split('://')[1].split('/')[0];
 	}
@@ -130,7 +133,7 @@ export default class {
 	}
 	connect_socket(no_ws = false) {
 		this.ws = new WebSocket(this.server);
-		this.ws.binaryType = "arraybuffer"
+		if (typeof BSON !== 'undefined') this.ws.binaryType = "arraybuffer"
 		this.ws.onopen = () => {
 			this.connected = true;
 			this.emit_event('connect', { server: this.server });
@@ -156,17 +159,6 @@ export default class {
 			this.emit_event(data.cmd, data.data)
 		};
 		return this;
-	}
-	ab2str(buf) {
-		return String.fromCharCode.apply(null, new Uint16Array(buf));
-	}
-	str2ab(str) {
-		var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-		var bufView = new Uint16Array(buf);
-		for (var i = 0, strLen = str.length; i < strLen; i++) {
-			bufView[i] = str.charCodeAt(i);
-		}
-		return buf;
 	}
 	send(data) {
 		if (data.cmd === 'connect') return this.connect(data.data);
